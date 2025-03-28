4add5383e06b1f6a875e9682688aaa843fddd10f
Nikolai Makarov <makarov.nikolai20@gmail.com>
2025-03-28 09:21:13 +0300

    an option to re-use old weights added

--- irbis_classifier/src/models/utils.py
+++ irbis_classifier/src/models/utils.py
@@ -8,7 +8,26 @@ from torch import nn
 def replace_last_linear(
     module: nn.Module,
     n_classes: int,
+    reuse_previous_weights: bool = False
 ) -> nn.Module:
+    """
+    This function finds last (the closest one to the model output layer) linear layer and replaces it
+    with the new one that has specified number of out_features.
+
+    Args:
+        module (nn.Module): model which last linear layer you want to replace.
+        n_classes (int): number of out_features new layer will have.
+        reuse_previous_weights (bool, optional): some of the new layer's weight will be initialized \
+            used previous layer's weight. Defaults to False.
+
+    Raises:
+        ValueError: if reuse_previous_weights is True, but old layer has more out_features than the new one \
+        an error will be raised.
+
+    Returns:
+        nn.Module: model copy with re-initialized last linear layer.
+    """
+
     if isinstance(module, nn.Linear):
         in_features: int = module.in_features
 
@@ -38,6 +57,27 @@ def replace_last_linear(
 
     if last_linear_name is not None and last_linear_parent is not None:
         in_features = getattr(last_linear_parent, last_linear_name).in_features
-        setattr(last_linear_parent, last_linear_name, nn.Linear(in_features, n_classes))
+
+        new_layer: nn.Linear = nn.Linear(
+            in_features,
+            n_classes,
+        )
+
+        if reuse_previous_weights:
+            previous_layer: nn.Linear = getattr(last_linear_parent, last_linear_name)
+            previous_n_classes: int = previous_layer.out_features
+
+            if previous_n_classes > n_classes:
+                raise ValueError(
+                    "can't reuse previously trained weights when" + \
+                    "new number of classes is smaller the the previous one",
+                )
+            new_layer.weight.data[:previous_n_classes, :] = previous_layer.weight.data
+
+        setattr(
+            last_linear_parent,
+            last_linear_name,
+            new_layer,
+        )
 
     return module_copy
